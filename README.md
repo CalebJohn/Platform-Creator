7ad24a22c47d1d0b4d00ee08531f03044e6bf9e7
Caleb John <calebjohn4@gmail.com>
2012-11-24 11:08:18 -0700

    eraser#11 bugs #3 #4

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Platform-Creator
 ================
 
-An android application for designing a 2d platformer level written in python using the Kivy framework.
+An android application for designing a 2d platformer level
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import kivy
 kivy.require('1.4.1')
 from kivy.core.window import Window
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.togglebutton import ToggleButton
 from kivy.clock import Clock
 from kivy.graphics import Line, Color, Ellipse, Rectangle, Point
+from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
-from math import fabs
 width, height = Window.size
 # Window.clearcolor = (1, 1, 1, 1)
 
@@ -16,7 +17,7 @@ def set_zoom(lines, zoom=48): #repopulates lists based on zoom level
     lat = range(0,width, zoom)
     long = range(0, height, zoom)
     for line in lines:
-        snap_to(line, zoom)
+        snap_to(line, zoom, lat)
     return lat, long, lines
     
 def snap_to(lines, zoom, lat): #uses the remainder between the point and the zoom to calculate where the point should snap to
@@ -47,9 +48,31 @@ class Platform_draw(Widget):
             self.color = (0.5, 0.25, 0.25)
             
     def undo(self, instance):  #removes the last line placed and then redraws the screen
-        if self.straight:
+        if not self.draw:
             self.lines = self.lines[:-1]
         self.refresh_screen()
+        
+    def load(self, instance):
+        file = open(self.directory + '/' + 'outline.map', 'r')
+        for points in file.read().split('\n'):
+            self.lines.append([Line(points = points, width = 2), (1,1,1)])
+        file.close()
+        self.refresh_screen()
+    def save(self, instance):
+        file = open(self.directory + '/' + 'outline.map', 'w')
+        for line in range(len(self.lines)):
+            file.write(str(self.lines[line][0].points[0]) + str(self.lines[line][0].points[1]) + str(self.lines[line][0].points[2]) + str(self.lines[line][0].points[3]) + '\n')
+        file.close()
+    def save_load(self, instance):
+        self.content = BoxLayout(orientation = 'vertical')
+        savebtn = Button(text = 'Save')
+        loadbtn = Button(text = 'Load')
+        self.content.add_widget(savebtn)
+        self.content.add_widget(loadbtn)
+        savebtn.bind(on_release = self.save)
+        loadbtn.bind(on_release = self.load)
+        self.popup = Popup(text = 'Save/Load', content = self.content, size_hint = (0.5, 0.2))
+        self.popup.open()
         
     def toggle_draw(self, instance): #stops the line from snapping to grid
         self.straight = False
@@ -75,12 +98,13 @@ class Platform_draw(Widget):
     def on_startup(self): #defines everything that needs to be defined before startup
         #defines variables to be used
         self.lines = []
-        self.draw_lines = []
+        self.coord = [[0,0]]
         self.drawn_lines = []
         self.straight = True
         self.draw = False
         self.scroll = False
         self.grab = False
+        self.straight_type = 'single'
         self.lat = ()
         self.long = ()
         self.zoom = height/12
@@ -88,16 +112,17 @@ class Platform_draw(Widget):
         self.color = (1, 1, 1)
         
         #creates all buttons used
-        self.btnw = Button(text = 'White', pos = (0, self.zoom), size = (self.zoom, self.zoom), background_color = (1, 1, 1, 0.9))
-        self.btnr = Button(text = 'Red', pos = (self.zoom, self.zoom), size = (self.zoom, self.zoom), background_color = (1, 0, 0, 0.9))
-        self.btnb = Button(text = 'Blue', pos = (self.zoom*2, self.zoom), size = (self.zoom, self.zoom), background_color = (0, 0, 1, 0.9))
-        self.btny = Button(text = 'Yellow', pos = (self.zoom*3, self.zoom), size = (self.zoom, self.zoom), background_color = (1, 1, 0, 0.9))
-        self.btng = Button(text = 'Green', pos = (self.zoom*4, self.zoom), size = (self.zoom, self.zoom), background_color = (0, 1, 0, 0.9))
-        self.btnbr = Button(text = 'Brown', pos = (self.zoom*5, self.zoom), size = (self.zoom, self.zoom), background_color = (1, 0.5, 0.5, 1))
+        self.btnw = Button(text = 'White', pos = (0, self.zoom), size = (self.zoom, self.zoom), background_color = (1, 1, 1, 0.65))
+        self.btnr = Button(text = 'Red', pos = (self.zoom, self.zoom), size = (self.zoom, self.zoom), background_color = (1, 0, 0, 0.65))
+        self.btnb = Button(text = 'Blue', pos = (self.zoom*2, self.zoom), size = (self.zoom, self.zoom), background_color = (0, 0, 1, 0.65))
+        self.btny = Button(text = 'Yellow', pos = (self.zoom*3, self.zoom), size = (self.zoom, self.zoom), background_color = (1, 1, 0, 0.65))
+        self.btng = Button(text = 'Green', pos = (self.zoom*4, self.zoom), size = (self.zoom, self.zoom), background_color = (0, 1, 0, 0.65))
+        self.btnbr = Button(text = 'Brown', pos = (self.zoom*5, self.zoom), size = (self.zoom, self.zoom), background_color = (1, 0.5, 0.5, 0.65))
+        self.btnsave = Button(text = 'Save', pos = (0, 0), size = (self.zoom, self.zoom))
         self.btnline = ToggleButton(state = 'down', group = 'state', background_normal = 'images/line.png', background_down = 'images/lined.png', pos = (self.zoom, 0), size = (self.zoom, self.zoom))
         self.btnm = ToggleButton(group = 'state', background_normal = 'images/4_point.png', background_down = 'images/4_pointd.png', pos = (self.zoom*2, 0), size = (self.zoom, self.zoom))
         self.btnc = ToggleButton(group = 'state', background_normal = 'images/cursor.png', background_down = 'images/cursord.png', pos = (self.zoom*3, 0), size = (self.zoom, self.zoom))
-        self.btndraw = ToggleButton(group = 'state', text = 'Draw', pos = (self.zoom*4, 0), size = (self.zoom, self.zoom))
+        self.btndraw = ToggleButton(group = 'state', background_normal = 'images/draw.png',background_down = 'images/drawd.png',  pos = (self.zoom*4, 0), size = (self.zoom, self.zoom))
         self.btnback = Button(background_normal = 'images/back.png', background_down = 'images/backd.png', pos = (self.zoom*5, 0), size = (self.zoom, self.zoom))
         
         #binds buttons to various functions
@@ -107,6 +132,7 @@ class Platform_draw(Widget):
         self.btny.bind(on_release = self.color_change)
         self.btng.bind(on_release = self.color_change)
         self.btnbr.bind(on_release = self.color_change)
+        self.btnsave.bind(on_release = self.save_load)
         self.btnback.bind(on_release = self.undo)
         self.btndraw.bind(on_release = self.toggle_draw)
         self.btnline.bind(on_release = self.toggle_straight)
@@ -131,6 +157,9 @@ class Platform_draw(Widget):
         
         #draws lines that the user has drawn
         for line in self.lines:
+            self.canvas.add(Color(*line[1]))
+            self.canvas.add(line[0])
+        for line in self.drawn_lines:
             self.canvas.add(Color(*line[1]))
             self.canvas.add(line[0])
             
@@ -161,8 +190,15 @@ class Platform_draw(Widget):
                 self.x, self.y = touch.x, touch.y #sets the start point of line
             if not self.coord:
                 self.coord = [[touch.x, touch.y]] #used as a start point if finger just entered screen
+            self.refresh_screen()
             self.canvas.add(Color(*self.color))
-            line = Line(points = (self.x, self.y, touch.x, touch.y), width = 1)  #draws a line that follows the users finger
+            coord_temp = [[self.x, self.y], [touch.x, touch.y]]
+            if self.straight_type == 'single' and self.straight:
+                coord_temp[0] = self.coord[0]
+                coord_temp = snap_to(coord_temp, self.zoom, self.lat)
+            line = Line(points = (coord_temp[0][0], coord_temp[0][1], coord_temp[1][0], coord_temp[1][1]), width = 1)  #draws a line that follows the users finger
+            if self.draw:
+                self.drawn_lines.append([line, self.color])
             self.canvas.add(line)
             self.x, self.y = touch.x, touch.y #updates points
              
@@ -199,6 +235,7 @@ class PlatformApp(App):
         self.main = Platform_draw()
         self.main.on_startup()
         self.main.lat, self.main.long, self.main.lines = set_zoom([], self.main.zoom)
+        self.main.directory = self.directory
         
         #draws grid lines
         for y in self.main.long:
@@ -206,18 +243,18 @@ class PlatformApp(App):
             self.parent.canvas.add(Line(points = (0, y, width, y), width = 1))
         btncolor = ToggleButton(group = 'a', text = 'Color', pos = (0, 0), size = (self.main.zoom, self.main.zoom))
         
+        #adds the self.main class
+        # btncolor.bind(on_release = self.color_toggle)
+        self.parent.add_widget(self.main)
+        
         # adds buttons to screen
         # self.parent.add_widget(btncolor)
+        self.parent.add_widget(self.main.btnsave)
         self.parent.add_widget(self.main.btnm)
         self.parent.add_widget(self.main.btnc)
         self.parent.add_widget(self.main.btndraw)
         self.parent.add_widget(self.main.btnback)
         self.parent.add_widget(self.main.btnline)
-        
-        
-        #adds the self.main class
-        # btncolor.bind(on_release = self.color_toggle)
-        self.parent.add_widget(self.main)
         
         #adds color buttons
         self.parent.add_widget(self.main.btnw)
